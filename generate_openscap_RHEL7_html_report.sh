652113049671013ea0a650d319a3dd89731b2f39
AchieveGoals <82723119+AchieveGoals@users.noreply.github.com>
2022-12-15 14:58:19 -0500

    Create generate_openscap_RHEL7_html_report.sh

---
+++ generate_openscap_RHEL7_html_report.sh
@@ -0,0 +1,32 @@
+# I use bash <filename>, you can use start your bash script as you prefer
+
+# Name: generate_openscap_RHEL6_HTML_report.sh
+# Larry Timmins  / gh/AchieveGoals contributor
+
+
+## housekeeping
+alias currdate="date +%Y%m%d-%H%M%S"
+
+## 
+cd
+if [ -d ~/scans]; then
+  cd scans
+else 
+  mkdir ~/scans
+  cd ~/scans
+fi 
+
+# remove prior file
+rm ~/scans/rhel-7.oval.xml
+
+# get latest
+wget -O -f https://www.redhat.com/security/data/oval/v2/RHEL7/rhel7-oval.xml.bz2 | bzip2 --decompress > rhel-7.oval.xml
+
+# run oscal and generate (after some time) the the HTML report
+oscal oval eval --report vulnerability-$(currdate)-$(hostname -s).html thel-7.oval.xml
+
+mail -s "openscap-scanner results (post RHSA-$(currdate)) - $(hostname -s)" -a $(ls -t1 vul*html) your.email@gmail.com
+
+# hit enter, type optional message and then hit Ctrl-D to end email
+
+
