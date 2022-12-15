1daf1419cc3ae54cf41c80e338fb3a62fc6585c0
AchieveGoals <82723119+AchieveGoals@users.noreply.github.com>
2022-12-15 15:18:47 -0500

    fix 2nd set of syntax errors

--- generate_openscap_RHEL7_html_report.sh
+++ generate_openscap_RHEL7_html_report.sh
@@ -5,7 +5,7 @@
 
 
 ## housekeeping
-alias currdate="date +%Y%m%d-%H%M%S"
+alias rhcurrdate="date +%Y%m%d-%H%M%S"
 
 ## 
 cd
@@ -20,12 +20,12 @@ fi
 rm ~/scans/rhel-7.oval.xml
 
 # get latest
-wget -O - https://www.redhat.com/security/data/oval/v2/RHEL7/rhel7-oval.xml.bz2 | bzip2 --decompress > rhel-7.oval.xml
+wget -O - https://www.redhat.com/security/data/oval/v2/RHEL7/rhel-7.oval.xml.bz2 | bzip2 --decompress > rhel-7.oval.xml
 
 # run oscal and generate (after some time) the the HTML report
-oscap oval eval --report vulnerability-$(currdate)-$(hostname -s).html rhel-7.oval.xml
+oscap oval eval --report vulnerability-$(rhcurrdate)-$(hostname -s).html rhel-7.oval.xml
 
-mail -s "openscap-scanner results (post RHSA-$(currdate)) - $(hostname -s)" -a $(ls -t1 vul*html) your.email@gmail.com
+mail -s "openscap-scanner results (post RHSA-$(rhcurrdate)) - $(hostname -s)" -a $(ls -t1 vul*html) your.email@gmail.com
 
 # hit enter, type optional message and then hit Ctrl-D to end email
 
